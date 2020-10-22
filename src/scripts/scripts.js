b28c33ceb2375e1610d0b0e83eb1eadd9fa712dd
JacobsChris <58259285+JacobsChris@users.noreply.github.com>
2020-10-22 14:41:43 +0100

    more psudoe

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -14,8 +14,15 @@ function createMultiButtons(timezones){
     // make buttons somehow
     document.createElement('button')
      .onClick(() => {
-         const res = jQuery.getJSON("http://worldtimeapi.org/api/timezone/" + timezone )
+         const res = jQuery.getJSON("http://worldtimeapi.org/api/timezone/" + timezone );
+         makeCityButtons(region)
          }
      )
 }
 
+function makeCityButtons(region) {
+    // same as above but cities
+     .onclick(()=>{
+        const res = jQuery.getJSON("http://worldtimeapi.org/api/timezone/" + timezone + "/" + city);
+        })
+}
